0a6e8dd685e7411e39f79b7d7d7fa584d7f80477
addthoriq <alvarro.thoriq@gmail.com>
2019-12-05 20:19:15 +0700

    template 1

---
+++ template/script.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="template/adminlte2/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="template/adminlte2/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="template/adminlte2/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="template/adminlte2/dist/js/demo.js"></script>
