5ba54ce17f45ab1dc8f902057c820b9d90100f30
addthoriq <alvarro.thoriq@gmail.com>
2019-12-09 09:15:26 +0700

    adminlte2

--- index.php
+++ index.php
@@ -1,86 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php
-        include 'template/link.php';
-    ?>
+    <?php include 'template/link.php';?>
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition skin-blue sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
-  <!-- Navbar -->
-    <?php
-        include 'template/header.php';
-    ?>
-  <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-    <?php
-        include 'template/sidebar.php';
-    ?>
+    <?php include 'template/header.php';?>
+  <!-- =============================================== -->
+
+  <!-- Left side column. contains the sidebar -->
+    <?php include 'template/sidebar.php';?>
+  <!-- =============================================== -->
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Belajar GIT</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
+      <h1>
+        Belajar Git
+        <small>it all starts here</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+      </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
 
       <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Title</h3>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
           </div>
         </div>
-        <div class="card-body">
+        <div class="box-body">
           Start creating your amazing application!
         </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
+        <!-- /.box-body -->
+        <div class="box-footer">
           Footer
         </div>
-        <!-- /.card-footer-->
+        <!-- /.box-footer-->
       </div>
-      <!-- /.card -->
+      <!-- /.box -->
 
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
-  <!-- Footer -->
-    <?php
-        include 'template/footer.php';
-    ?>
+    <?php include 'template/footer.php'; ?>
 
+  <!-- Control Sidebar -->
 </div>
 <!-- ./wrapper -->
 
-    <!-- Javascript -->
-        <?php
-            include 'template/script.php';
-        ?>
-
+    <?php include 'template/script.php';?>
 </body>
 </html>
--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-  <div class="float-right d-none d-sm-block">
-    <b>Version</b> 1.0
+  <div class="pull-right hidden-xs">
+    <b>Version</b> 2.4.0
   </div>
-  <strong>Copyright &copy; 2019 <a href="http://www.instagram.com/addthoriq">Addthoriq</a>.</strong> All rights
+  <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
   reserved.
 </footer>
